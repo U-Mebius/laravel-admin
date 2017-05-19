9715f2c09ffbf7aec8552877eda987ec64d11a29
Dakeyama <dakeyama@umebius.com>
2017-05-19 17:07:00 +0900

    change composer name

--- views/partials/footer.blade.php
+++ views/partials/footer.blade.php
@@ -5,5 +5,5 @@
         <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
     </div>
     <!-- Default to the left -->
-    <strong>Copyright &copy; 2016 <a href="https://github.com/z-song/laravel-admin">laravel-admin</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; {{ date('Y') }} {{ config('admin.name') }}.</strong> All rights reserved.
 </footer>
